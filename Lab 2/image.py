8732e02f2ddff4191a6ca187ead4b9d8eee761b2
JacobEverly <Je354@cornell.edu>
2022-09-11 17:14:53 -0400

    Clock Code

--- Lab 2/image.py
+++ Lab 2/image.py
@@ -10,7 +10,9 @@ not support PIL/pillow (python imaging library)!
 
 Author(s): Melissa LeBlanc-Williams for Adafruit Industries
 """
-
+import time
+from time import sleep
+from datetime import datetime
 import digitalio
 import board
 from PIL import Image, ImageDraw
@@ -57,45 +59,72 @@ disp = st7789.ST7789(
 )
 # pylint: enable=line-too-long
 
-# Create blank image for drawing.
-# Make sure to create image with mode 'RGB' for full color.
-if disp.rotation % 180 == 90:
-    height = disp.width  # we swap height/width to rotate it to landscape!
-    width = disp.height
-else:
-    width = disp.width  # we swap height/width to rotate it to landscape!
-    height = disp.height
-image = Image.new("RGB", (width, height))
 
-# Get drawing object to draw on image.
-draw = ImageDraw.Draw(image)
+# define what day of the week it is
+#dt = datetime.now()
+#day = dt.isoweekday()
 
-# Draw a black filled box to clear the image.
-draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
-disp.image(image)
+#select appropiate image based on the day of the week
+days = [1,2,3,4,5,6,7]
 
-image = Image.open("red.jpg")
-backlight = digitalio.DigitalInOut(board.D22)
-backlight.switch_to_output()
-backlight.value = True
+for day in days:
+
+    if day == 1 :
+        emotion = "Monday.jpg"
+    elif day == 2:
+        emotion = "Tuesday.jpg"
+    elif day == 3: 
+        emotion = "Wednesday.jpg"
+    elif day == 4:
+        emotion = "Thursday.jpg"
+    elif day == 5:
+        emotion = "Friday.jpg"
+    elif day == 6:
+        emotion = "Saturday.jpg"
+    else:
+        emotion = "Sunday.jpg"
+
+    # Create blank image for drawing.
+    # Make sure to create image with mode 'RGB' for full color.
+    if disp.rotation % 180 == 90:
+        height = disp.width  # we swap height/width to rotate it to landscape!
+        width = disp.height
+    else:
+        width = disp.width  # we swap height/width to rotate it to landscape!
+        height = disp.height
+    image = Image.new("RGB", (width, height))
+
+    # Get drawing object to draw on image.
+    draw = ImageDraw.Draw(image)
+
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+    disp.image(image)
 
 
-# Scale the image to the smaller screen dimension
-image_ratio = image.width / image.height
-screen_ratio = width / height
-if screen_ratio < image_ratio:
-    scaled_width = image.width * height // image.height
-    scaled_height = height
-else:
-    scaled_width = width
-    scaled_height = image.height * width // image.width
-image = image.resize((scaled_width, scaled_height), Image.BICUBIC)
+    image = Image.open(emotion)
+    backlight = digitalio.DigitalInOut(board.D22)
+    backlight.switch_to_output()
+    backlight.value = True
 
-# Crop and center the image
-x = scaled_width // 2 - width // 2
-y = scaled_height // 2 - height // 2
-image = image.crop((x, y, x + width, y + height))
 
-# Display image.
-disp.image(image)
+    # Scale the image to the smaller screen dimension
+    image_ratio = image.width / image.height
+    screen_ratio = width / height
+    if screen_ratio < image_ratio:
+        scaled_width = image.width * height // image.height
+        scaled_height = height
+    else:
+        scaled_width = width
+        scaled_height = image.height * width // image.width
+    image = image.resize((scaled_width, scaled_height), Image.BICUBIC)
+
+    # Crop and center the image
+    x = scaled_width // 2 - width // 2
+    y = scaled_height // 2 - height // 2
+    image = image.crop((x, y, x + width, y + height))
+
+    # Display image.
+    disp.image(image)
+    time.sleep(1)
 
